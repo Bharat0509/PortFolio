bed16bdd8595e401c333f8e02a0e72ee72f0c58b
Bharat Bhammar <bhartbhammar3336@gmail.com>
2023-04-04 16:14:38 +0530

    New Project Added

--- src/components/portfolioProjects/PortfolioProjects.jsx
+++ src/components/portfolioProjects/PortfolioProjects.jsx
@@ -11,14 +11,39 @@ const Portfolio = () => {
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
       <div className="container portfolio__container">
-      <article className='portfolio__item' >
+
+
+        <article className='portfolio__item' >
+          <div className="portfolio__item-image">
+            <img src='./JustSocials.png' alt="project-banner" />
+          </div>
+          <h3>JustSocials - a social media site</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/Bharat0509/BharatSocial" className='btn'>Github</a>
+            <a href="https://bharatsocial-ppi2.onrender.com/" className='btn btn-primary' target='_blank '>Live Demo</a>
+          </div>
+        </article>
+
+        <article className='portfolio__item' >
+          <div className="portfolio__item-image">
+            <img src='./BetterMart.png' alt="project-banner" />
+          </div>
+          <h3>BetterMart - an e-commerce site</h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com/Bharat0509/BharatEcome" className='btn'>Github</a>
+            <a href="https://bharatecommerce.onrender.com/" className='btn btn-primary' target='_blank '>Live Demo</a>
+          </div>
+        </article>
+
+
+        <article className='portfolio__item' >
           <div className="portfolio__item-image">
             <img src={IMG1} alt="project-banner" />
           </div>
           <h3>Youtube Clone</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/bharat0509" className='btn'>Github</a>
-          <a href="https://github.com" className='btn btn-primary' target='_blank '>Live Demo</a>
+            <a href="https://github.com/bharat0509" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank '>Live Demo</a>
           </div>
         </article>
 
@@ -28,19 +53,19 @@ const Portfolio = () => {
           </div>
           <h3>MoviesWatch- A Movies Details App</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/bharat0509" className='btn'>Github</a>
-          <a href="https://github.com" className='btn btn-primary' target='_blank '>Live Demo</a>
+            <a href="https://github.com/bharat0509" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank '>Live Demo</a>
           </div>
         </article>
 
         <article className='portfolio__item' >
           <div className="portfolio__item-image">
-            <img src={IMG3} alt="project-banner" />
+            <img src={IMG4} alt="project-banner" />
           </div>
-          <h3>BharatSocials- A Social Media App</h3>
+          <h3>LastMomentBuddy - an exam helper site</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/bharat0509" className='btn'>Github</a>
-          <a href="https://github.com" className='btn btn-primary' target='_blank '>Live Demo</a>
+            <a href="https://github.com/Bharat0509/LastMomentBuddy" className='btn'>Github</a>
+            <a href="https://lastmomentbuddy.onrender.com/" className='btn btn-primary' target='_blank '>Live Demo</a>
           </div>
         </article>
 
@@ -50,8 +75,8 @@ const Portfolio = () => {
           </div>
           <h3>Todo List-A Personal Todo App</h3>
           <div className="portfolio__item-cta">
-          <a href="https://github.com/bharat0509" className='btn'>Github</a>
-          <a href="https://github.com" className='btn btn-primary' target='_blank '>Live Demo</a>
+            <a href="https://github.com/bharat0509" className='btn'>Github</a>
+            <a href="https://github.com" className='btn btn-primary' target='_blank '>Live Demo</a>
           </div>
         </article>
       </div>
